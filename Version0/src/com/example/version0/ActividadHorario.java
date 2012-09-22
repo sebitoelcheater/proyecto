979913fe15cb2bf7a562d763f34826bec0745729
sking32 <el.ariel.cl@gmail.com>
2012-09-22 12:58:26 -0300

    Un ultimo arreglo a la interfaz gráfica

--- Version0/src/com/example/version0/ActividadHorario.java
+++ Version0/src/com/example/version0/ActividadHorario.java
@@ -3,13 +3,18 @@ package com.example.version0;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ActividadHorario extends Activity {
-
+	
+	String []diasDeLaSemana = {"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_horario);
+        Spinner numeroDeNotas = (Spinner)findViewById(R.id.spinner1);
+        numeroDeNotas.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,diasDeLaSemana));
     }
 
     @Override
