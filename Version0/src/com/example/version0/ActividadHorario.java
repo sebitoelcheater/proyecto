fcc0b4f4ad7587598899e4a18e845492e7838fc6
sking32 <el.ariel.cl@gmail.com>
2012-09-22 13:54:23 -0300

    Cree un peuqeño programita el cual ya se puede conectar con la DB :)

--- Version0/src/com/example/version0/ActividadHorario.java
+++ Version0/src/com/example/version0/ActividadHorario.java
@@ -1,20 +1,39 @@
 package com.example.version0;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
 
-public class ActividadHorario extends Activity {
+public class ActividadHorario extends Activity implements OnClickListener{
 	
 	String []diasDeLaSemana = {"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};
-    @Override
+    ArrayList<String> array_modulos = new ArrayList<String>();
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_horario);
+        
         Spinner numeroDeNotas = (Spinner)findViewById(R.id.spinner1);
         numeroDeNotas.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,diasDeLaSemana));
+       
+        
+        //Esto puede ser un actualizable
+        /*ListView modulos = (ListView)findViewById(R.id.listView1);
+        modulos.setAdapter(new ArrayAdapter<Integer>(this,android.R.layout.simple_list_item_1,array_modulos));
+        */
+        
+        Button ver = (Button)findViewById(R.id.button1);
+        ver.setOnClickListener(this);
+       
     }
 
     @Override
@@ -22,4 +41,21 @@ public class ActividadHorario extends Activity {
         getMenuInflater().inflate(R.menu.activity_actividad_horario, menu);
         return true;
     }
+
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		Spinner numeroDeNotas = (Spinner)findViewById(R.id.spinner1);
+		generaModulos(numeroDeNotas.getSelectedItemPosition());
+		
+		ListView modulos = (ListView)findViewById(R.id.listView1);
+        modulos.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,array_modulos));
+	}
+
+	private void generaModulos(int selectedItemPosition) {
+		// TODO Auto-generated method stub
+		array_modulos = new ArrayList<String>();
+		//Aca se supone que va la coneccion con la DB
+		for(int i = 0 ; i<=selectedItemPosition; ++i)
+			array_modulos.add("Modulo"+(i+1));
+	}
 }
