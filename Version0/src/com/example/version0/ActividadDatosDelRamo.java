332b14d789330a57d05ab85f37d944113158bf00
Esteban Iglesias M <estebaniglesiasmanriquez@gmail.com>
2012-11-13 14:22:51 -0300

    Ahora se actualiza el ramo en modo VER cuando luego se edita

--- Version0/src/com/example/version0/ActividadDatosDelRamo.java
+++ Version0/src/com/example/version0/ActividadDatosDelRamo.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
@@ -23,10 +24,14 @@ import android.widget.ListView;
 import com.example.controlador.Controlador;
 import com.example.controlador.Curso;
 import com.example.controlador.Modulo;
+import com.example.version0.ActividadRamos.MiArrayAdapter;
 
 
 
 public class ActividadDatosDelRamo extends ListActivity implements OnItemClickListener {
+    private static final int REQUEST_EDITAR_O_AGREGAR = 0;
+    private String idRamoAEditar = null;
+
 	
 	public class MiModuloArrayAdapter extends ArrayAdapter<Modulo> {
 
@@ -37,7 +42,13 @@ public class ActividadDatosDelRamo extends ListActivity implements OnItemClickLi
 				this.objects = listaModulos;
 			}
 		
+		  public void agregarModulo(Modulo modulo) {
+	            objects.add(modulo);
+	        }
 
+	        public void clear() {
+	            objects.clear();
+	        }
 
 		  @Override
 		  public View getView(final int position, View convertView, ViewGroup parent) {
@@ -56,7 +67,8 @@ public class ActividadDatosDelRamo extends ListActivity implements OnItemClickLi
 		  }
 		} 
 
-	
+	private MiModuloArrayAdapter adaptador;
+
 	public Curso cursoAVer;
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -64,7 +76,7 @@ public class ActividadDatosDelRamo extends ListActivity implements OnItemClickLi
         //Recibe el mensaje del inetent
         Intent intent = getIntent();
         //Aquí recibe la id (como string) del ramo a editar
-        String idRamoAEditar = intent.getStringExtra("id");
+        idRamoAEditar = intent.getStringExtra("id");
         
         cursoAVer = new Curso(this,idRamoAEditar);
        
@@ -86,8 +98,9 @@ public class ActividadDatosDelRamo extends ListActivity implements OnItemClickLi
 
         campoTextoNombre.setText(nombreOriginal);
         
-        setListAdapter(new MiModuloArrayAdapter(this, R.layout.item_modulo, array_modulos));
-
+        
+        adaptador = new MiModuloArrayAdapter(this, R.layout.item_modulo, array_modulos);
+        setListAdapter(adaptador);
         
         
     }
@@ -107,9 +120,52 @@ public class ActividadDatosDelRamo extends ListActivity implements OnItemClickLi
 	
 	public void editarRamo(View view)
     {
-		Intent intent = new Intent(this,ActividadEdicionRamo.class);
-	 	intent.putExtra("id", cursoAVer.obtenerId());
-	 	startActivity(intent);
+		Intent intentEditarRamo = new Intent(this,ActividadEdicionRamo.class);
+	 	intentEditarRamo.putExtra("id", cursoAVer.obtenerId());
+     	startActivityForResult(intentEditarRamo, REQUEST_EDITAR_O_AGREGAR);
+    }
+	
+	public void actualizarDatosRamo() {
+		/*Una vez modificados los datos a partir de esta actividad,
+		 * es necesario actualizar los datos del Ramo*/
+		//Primero actualiza el nombre
+        cursoAVer = new Curso(this,idRamoAEditar);
+
+        String nombreOriginal = cursoAVer.obtenerNombre(); //Re-obtiene el nombre del ramo
+        /*Este método se utiliza gracias a que obtenerNombre() se conecta con la base de datos
+         * la cual ya está actualizada*/
+        TextView campoTextoNombre = (TextView) findViewById(R.id.nombreRamoAVer);
+        campoTextoNombre.setText(nombreOriginal);
+        
+        ArrayList<Modulo> nuevo_array_modulos = Controlador.obtenerModulosPorIdCurso(this, idRamoAEditar);
+
+        
+        adaptador.clear();
+        for(Modulo modulo : nuevo_array_modulos){
+        	adaptador.agregarModulo(modulo);
+        }
+        adaptador.notifyDataSetChanged();
+
+
+       
+    }
+	
+	@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+        /*Cuando se llega de un intent, se devuelve un "resultado". Según este resultado
+         * se realizan varias acciones.
+         * En este caso el único resultado por ahora es un OK para EDITAR O AGREGAR un Curso
+         * y en este caso, actualiza la Lista de Ramos*/
+        case REQUEST_EDITAR_O_AGREGAR:
+            if (resultCode == RESULT_OK) {
+            	//Método necesario para actualizar vizualmente la lista de los Ramos
+                actualizarDatosRamo();
+                getWindow()
+                        .setSoftInputMode(
+                                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+            }
+        }
     }
 	
 }
