30d5cf69b4a611d7d1e2f4efe33b2096265e3946
sebakan <seba.hg@me.com>
2012-11-19 00:09:38 -0300

    instalación de TabBar a la versión 2

--- version2/gen/com/example/version2/R.java
+++ version2/gen/com/example/version2/R.java
@@ -8,43 +8,81 @@
 package com.example.version2;
 
 public final class R {
+    public static final class array {
+        public static final int Spinner_Content=0x7f070000;
+    }
     public static final class attr {
     }
+    public static final class color {
+        public static final int tabDark=0x7f040001;
+        public static final int tabMedium=0x7f040000;
+        public static final int tabTransparent=0x7f040002;
+    }
+    public static final class dimen {
+        public static final int corner_radius=0x7f050000;
+        public static final int corner_radius_zero=0x7f050001;
+        public static final int tab_space=0x7f050003;
+        public static final int tab_space_bottom_line=0x7f050005;
+        public static final int tab_space_plus1=0x7f050004;
+        public static final int tab_space_top=0x7f050002;
+        public static final int tab_space_unselected_top=0x7f050006;
+    }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int app_background=0x7f020000;
+        public static final int camera=0x7f020001;
+        public static final int center_tab_focus=0x7f020002;
+        public static final int center_tab_indicator=0x7f020003;
+        public static final int center_tab_press=0x7f020004;
+        public static final int center_tab_selected=0x7f020005;
+        public static final int center_tab_unselected=0x7f020006;
+        public static final int home_normal=0x7f020007;
+        public static final int home_selected=0x7f020008;
+        public static final int ic_action_search=0x7f020009;
+        public static final int ic_launcher=0x7f02000a;
+        public static final int icon=0x7f02000b;
+        public static final int search_normal=0x7f02000c;
+        public static final int search_selected=0x7f02000d;
+        public static final int tab_focus=0x7f02000e;
+        public static final int tab_home=0x7f02000f;
+        public static final int tab_indicator=0x7f020010;
+        public static final int tab_press=0x7f020011;
+        public static final int tab_search=0x7f020012;
+        public static final int tab_selected=0x7f020013;
+        public static final int tab_unselected=0x7f020014;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f070012;
-        public static final int botonAceptarEliminarModulo=0x7f070011;
-        public static final int botonAgregarModulo=0x7f070005;
-        public static final int botonCambios=0x7f070006;
-        public static final int botonCancelarEliminarModulo=0x7f070010;
-        public static final int botonEditarEsteRamo=0x7f070003;
-        public static final int botonEditarModulo=0x7f070019;
-        public static final int botonEditarRamo=0x7f07001c;
-        public static final int botonEliminarModulo=0x7f07001a;
-        public static final int botonSuscribirCurso=0x7f070017;
-        public static final int button1=0x7f070009;
-        public static final int button2=0x7f070016;
-        public static final int diaModulo=0x7f07000c;
-        public static final int dospuntos=0x7f07000e;
-        public static final int editText1=0x7f070008;
-        public static final int edit_message=0x7f07000b;
-        public static final int horaFin=0x7f07000f;
-        public static final int horaInicio=0x7f07000d;
-        public static final int idCursoASuscribir2=0x7f070018;
-        public static final int listView1=0x7f07000a;
-        public static final int menu_settings=0x7f07001d;
-        public static final int nombreRamoAEditar=0x7f070004;
-        public static final int nombreRamoAVer=0x7f070001;
-        public static final int spinner1=0x7f070007;
-        public static final int textView1=0x7f070000;
-        public static final int textView2=0x7f070002;
-        public static final int textView3=0x7f070015;
-        public static final int textoNombreRamo=0x7f07001b;
-        public static final int timePicker1=0x7f070013;
-        public static final int timePicker2=0x7f070014;
+        public static final int RelativeLayout1=0x7f0a0013;
+        public static final int botonAceptarEliminarModulo=0x7f0a0012;
+        public static final int botonAgregarModulo=0x7f0a0005;
+        public static final int botonCambios=0x7f0a0006;
+        public static final int botonCancelarEliminarModulo=0x7f0a0011;
+        public static final int botonEditarEsteRamo=0x7f0a0003;
+        public static final int botonEditarModulo=0x7f0a001a;
+        public static final int botonEditarRamo=0x7f0a001d;
+        public static final int botonEliminarModulo=0x7f0a001b;
+        public static final int botonSuscribirCurso=0x7f0a0018;
+        public static final int button1=0x7f0a0009;
+        public static final int button2=0x7f0a0017;
+        public static final int diaModulo=0x7f0a000d;
+        public static final int dospuntos=0x7f0a000f;
+        public static final int editText1=0x7f0a0008;
+        public static final int edit_message=0x7f0a000b;
+        public static final int horaFin=0x7f0a0010;
+        public static final int horaInicio=0x7f0a000e;
+        public static final int icon=0x7f0a000c;
+        public static final int idCursoASuscribir2=0x7f0a0019;
+        public static final int listView1=0x7f0a000a;
+        public static final int menu_settings=0x7f0a001f;
+        public static final int nombreRamoAEditar=0x7f0a0004;
+        public static final int nombreRamoAVer=0x7f0a0001;
+        public static final int spinner1=0x7f0a0007;
+        public static final int textView1=0x7f0a0000;
+        public static final int textView2=0x7f0a0002;
+        public static final int textView3=0x7f0a0016;
+        public static final int textoNombreRamo=0x7f0a001c;
+        public static final int timePicker1=0x7f0a0014;
+        public static final int timePicker2=0x7f0a0015;
+        public static final int title=0x7f0a001e;
     }
     public static final class layout {
         public static final int activity_actividad_datos_del_ramo=0x7f030000;
@@ -54,56 +92,60 @@ public final class R {
         public static final int activity_actividad_principal=0x7f030004;
         public static final int activity_actividad_que_muestra_mensaje=0x7f030005;
         public static final int activity_actividad_ramos=0x7f030006;
-        public static final int dialogo_eliminar_modulo=0x7f030007;
-        public static final int dialogo_modulo=0x7f030008;
-        public static final int dialogo_suscribir_curso=0x7f030009;
-        public static final int item_modulo=0x7f03000a;
-        public static final int item_modulo_editando=0x7f03000b;
-        public static final int lista_ramos=0x7f03000c;
-        public static final int nuevo_ramo=0x7f03000d;
+        public static final int center_tab_indicator=0x7f030007;
+        public static final int dialogo_eliminar_modulo=0x7f030008;
+        public static final int dialogo_modulo=0x7f030009;
+        public static final int dialogo_suscribir_curso=0x7f03000a;
+        public static final int item_modulo=0x7f03000b;
+        public static final int item_modulo_editando=0x7f03000c;
+        public static final int lista_ramos=0x7f03000d;
+        public static final int main=0x7f03000e;
+        public static final int nuevo_ramo=0x7f03000f;
+        public static final int tab_indicator=0x7f030010;
     }
     public static final class menu {
-        public static final int activity_actividad_configuracion=0x7f060000;
-        public static final int activity_actividad_datos_del_ramo=0x7f060001;
-        public static final int activity_actividad_feedback=0x7f060002;
-        public static final int activity_actividad_horario=0x7f060003;
-        public static final int activity_actividad_ramos=0x7f060004;
-        public static final int activity_principal=0x7f060005;
+        public static final int activity_actividad_configuracion=0x7f090000;
+        public static final int activity_actividad_datos_del_ramo=0x7f090001;
+        public static final int activity_actividad_feedback=0x7f090002;
+        public static final int activity_actividad_horario=0x7f090003;
+        public static final int activity_actividad_ramos=0x7f090004;
+        public static final int activity_principal=0x7f090005;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int boton_configurar=0x7f04000c;
-        public static final int boton_feedback=0x7f040005;
-        public static final int boton_horario=0x7f04000d;
-        public static final int boton_notas=0x7f04000e;
-        public static final int button_send=0x7f040009;
-        public static final int calificaciones=0x7f040007;
-        public static final int configuraci_n_de_ramos=0x7f040008;
-        public static final int edit_message=0x7f040001;
-        public static final int edittextNombredelRamo=0x7f040018;
-        public static final int hello_world=0x7f04000a;
-        public static final int horario=0x7f040006;
-        public static final int menu_settings=0x7f040002;
-        public static final int nombre_ramo=0x7f040012;
-        public static final int nombre_ramo_con_otro_nombre=0x7f040013;
-        public static final int pagina_principal_=0x7f040004;
-        public static final int title_activity_actividad_configuracion=0x7f04000f;
-        public static final int title_activity_actividad_datos_del_ramo=0x7f040015;
-        public static final int title_activity_actividad_edicion_ramo=0x7f04001d;
-        public static final int title_activity_actividad_feedback=0x7f04001c;
-        public static final int title_activity_actividad_horario=0x7f040010;
-        public static final int title_activity_actividad_notas=0x7f040011;
-        public static final int title_activity_actividad_notas2=0x7f04001b;
-        public static final int title_activity_actividad_que_muestra_mensaje=0x7f04000b;
-        public static final int title_activity_actividad_ramos=0x7f040016;
-        public static final int title_activity_actividad_ramos2=0x7f04001a;
-        public static final int title_activity_actividad_suscribir_curso=0x7f04001e;
-        public static final int title_activity_datos_del_ramo=0x7f040014;
-        public static final int title_activity_dialogo=0x7f040019;
-        public static final int title_activity_nota_nombre_ramo=0x7f040017;
-        public static final int title_activity_principal=0x7f040003;
+        public static final int app_name=0x7f060000;
+        public static final int boton_configurar=0x7f06000c;
+        public static final int boton_feedback=0x7f060005;
+        public static final int boton_horario=0x7f06000d;
+        public static final int boton_notas=0x7f06000e;
+        public static final int button_send=0x7f060009;
+        public static final int calificaciones=0x7f060007;
+        public static final int configuraci_n_de_ramos=0x7f060008;
+        public static final int edit_message=0x7f060001;
+        public static final int edittextNombredelRamo=0x7f060018;
+        public static final int hello=0x7f06001f;
+        public static final int hello_world=0x7f06000a;
+        public static final int horario=0x7f060006;
+        public static final int menu_settings=0x7f060002;
+        public static final int nombre_ramo=0x7f060012;
+        public static final int nombre_ramo_con_otro_nombre=0x7f060013;
+        public static final int pagina_principal_=0x7f060004;
+        public static final int title_activity_actividad_configuracion=0x7f06000f;
+        public static final int title_activity_actividad_datos_del_ramo=0x7f060015;
+        public static final int title_activity_actividad_edicion_ramo=0x7f06001d;
+        public static final int title_activity_actividad_feedback=0x7f06001c;
+        public static final int title_activity_actividad_horario=0x7f060010;
+        public static final int title_activity_actividad_notas=0x7f060011;
+        public static final int title_activity_actividad_notas2=0x7f06001b;
+        public static final int title_activity_actividad_que_muestra_mensaje=0x7f06000b;
+        public static final int title_activity_actividad_ramos=0x7f060016;
+        public static final int title_activity_actividad_ramos2=0x7f06001a;
+        public static final int title_activity_actividad_suscribir_curso=0x7f06001e;
+        public static final int title_activity_datos_del_ramo=0x7f060014;
+        public static final int title_activity_dialogo=0x7f060019;
+        public static final int title_activity_nota_nombre_ramo=0x7f060017;
+        public static final int title_activity_principal=0x7f060003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f080000;
     }
 }
