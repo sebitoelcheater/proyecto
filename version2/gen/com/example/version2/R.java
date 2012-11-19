c8f51a9e6613823141d4542dbbdc7342caabcaf8
sebakan <seba.hg@me.com>
2012-11-19 00:41:05 -0300

    cambio de icono calendario

--- version2/gen/com/example/version2/R.java
+++ version2/gen/com/example/version2/R.java
@@ -29,26 +29,27 @@ public final class R {
     }
     public static final class drawable {
         public static final int app_background=0x7f020000;
-        public static final int camera=0x7f020001;
-        public static final int center_tab_focus=0x7f020002;
-        public static final int center_tab_indicator=0x7f020003;
-        public static final int center_tab_press=0x7f020004;
-        public static final int center_tab_selected=0x7f020005;
-        public static final int center_tab_unselected=0x7f020006;
-        public static final int home_normal=0x7f020007;
-        public static final int home_selected=0x7f020008;
-        public static final int ic_action_search=0x7f020009;
-        public static final int ic_launcher=0x7f02000a;
-        public static final int icon=0x7f02000b;
-        public static final int search_normal=0x7f02000c;
-        public static final int search_selected=0x7f02000d;
-        public static final int tab_focus=0x7f02000e;
-        public static final int tab_home=0x7f02000f;
-        public static final int tab_indicator=0x7f020010;
-        public static final int tab_press=0x7f020011;
-        public static final int tab_search=0x7f020012;
-        public static final int tab_selected=0x7f020013;
-        public static final int tab_unselected=0x7f020014;
+        public static final int calendario=0x7f020001;
+        public static final int camera=0x7f020002;
+        public static final int center_tab_focus=0x7f020003;
+        public static final int center_tab_indicator=0x7f020004;
+        public static final int center_tab_press=0x7f020005;
+        public static final int center_tab_selected=0x7f020006;
+        public static final int center_tab_unselected=0x7f020007;
+        public static final int home_normal=0x7f020008;
+        public static final int home_selected=0x7f020009;
+        public static final int ic_action_search=0x7f02000a;
+        public static final int ic_launcher=0x7f02000b;
+        public static final int icon=0x7f02000c;
+        public static final int search_normal=0x7f02000d;
+        public static final int search_selected=0x7f02000e;
+        public static final int tab_focus=0x7f02000f;
+        public static final int tab_home=0x7f020010;
+        public static final int tab_indicator=0x7f020011;
+        public static final int tab_press=0x7f020012;
+        public static final int tab_search=0x7f020013;
+        public static final int tab_selected=0x7f020014;
+        public static final int tab_unselected=0x7f020015;
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f0a0013;
